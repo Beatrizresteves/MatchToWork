1b91df01fadfb7c2ca5d1eeb7f105f528398595d
Beatrizresteves <Beatrizresteves@github.com>
2024-06-23 20:38:54 -0300

    Add rota de service

---
+++ routes/service_routes.py
@@ -0,0 +1,15 @@
+from flask import Blueprint
+from api.service_api import get_services, get_service, create_service, update_service, delete_service
+
+service_bp = Blueprint('service_api', __name__)
+
+service_bp.add_url_rule('/services', 'get_services',
+                        get_services, methods=['GET'])
+service_bp.add_url_rule('/services/<int:service_id>',
+                        'get_service', get_service, methods=['GET'])
+service_bp.add_url_rule('/services', 'create_service',
+                        create_service, methods=['POST'])
+service_bp.add_url_rule('/services/<int:service_id>',
+                        'update_service', update_service, methods=['PUT'])
+service_bp.add_url_rule('/services/<int:service_id>',
+                        'delete_service', delete_service, methods=['DELETE'])

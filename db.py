9a3a33af6c3412ffea81b77acc5e18bd070b71ba
Beatrizresteves <beatrizramalho.esteves@gmail.com>
2024-07-01 15:14:40 -0300

    Inclui update_at em tabelas

--- db.py
+++ db.py
@@ -1,7 +1,6 @@
 import psycopg2
 from config import Config
 
-
 def get_db_connection():
     conn = psycopg2.connect(
         dbname=Config.DB_NAME,
@@ -11,7 +10,6 @@ def get_db_connection():
         port=Config.DB_PORT,
     )
     return conn
-
 
 def init_db():
     conn = get_db_connection()
@@ -37,7 +35,9 @@ def init_db():
         CREATE TABLE IF NOT EXISTS servicetypes (
             service_type_id SERIAL PRIMARY KEY,
             name VARCHAR(80) NOT NULL,
-            description VARCHAR(200)
+            description VARCHAR(200),
+            created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+            update_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
         );
     ''')
 
@@ -49,6 +49,8 @@ def init_db():
             start_date TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
             end_date TIMESTAMPTZ,
             status VARCHAR(20) NOT NULL,
+            created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+            update_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
             FOREIGN KEY (service_type_id) REFERENCES servicetypes (service_type_id),
             FOREIGN KEY (client_id) REFERENCES users (user_id)
         );
@@ -56,11 +58,40 @@ def init_db():
         CREATE INDEX IF NOT EXISTS idx_service_end_date ON services (end_date);
     ''')
 
+    cur.execute('''
+        CREATE OR REPLACE FUNCTION update_update_at_column()
+        RETURNS TRIGGER AS $$
+        BEGIN
+            NEW.update_at = NOW();
+            RETURN NEW;
+        END;
+        $$ LANGUAGE plpgsql;
+    ''')
+
+    cur.execute('''
+        CREATE TRIGGER update_users_update_at
+        BEFORE UPDATE ON users
+        FOR EACH ROW
+        EXECUTE FUNCTION update_update_at_column();
+    ''')
+
+    cur.execute('''
+        CREATE TRIGGER update_servicetypes_update_at
+        BEFORE UPDATE ON servicetypes
+        FOR EACH ROW
+        EXECUTE FUNCTION update_update_at_column();
+    ''')
+
+    cur.execute('''
+        CREATE TRIGGER update_services_update_at
+        BEFORE UPDATE ON services
+        FOR EACH ROW
+        EXECUTE FUNCTION update_update_at_column();
+    ''')
+
     conn.commit()
     cur.close()
     conn.close()
-
-
 
 if __name__ == '__main__':
     init_db()

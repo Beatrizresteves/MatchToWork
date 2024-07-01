9b9546ad9878a2ddf3837aecfca7223da7a97003
Beatrizresteves <beatrizramalho.esteves@gmail.com>
2024-07-01 16:53:16 -0300

    Conserta models

--- models/service.py
+++ models/service.py
@@ -2,15 +2,17 @@ from datetime import datetime
 
 
 class Service:
-    def __init__(self, service_id, service_type_id, client_id, start_date=None, end_date=None, status=None):
+    def __init__(self, service_id, service_type_id, client_id, start_date=None, end_date=None, status=None,  created_at=None, updated_at=None):
         self.service_id = service_id
         self.service_type_id = service_type_id
         self.client_id = client_id
         self.start_date = start_date or datetime.utcnow()
         self.end_date = end_date
         self.status = status
+        self.created_at = created_at or datetime.utcnow()
+        self.updated_at = updated_at or datetime.utcnow()
 
     @classmethod
     def from_db_row(cls, row):
-        service_id, service_type_id, client_id, start_date, end_date, status = row
-        return cls(service_id, service_type_id, client_id, start_date, end_date, status)
+        service_id, service_type_id, client_id, start_date, end_date, created_at, updated_at, status = row, 
+        return cls(service_id, service_type_id, client_id, start_date, end_date, status, created_at, updated_at)
--- models/servicetype.py
+++ models/servicetype.py
@@ -1,10 +1,13 @@
+from datetime import datetime
 class ServiceType:
-    def __init__(self, service_type_id, name, description=None):
+    def __init__(self, service_type_id, name,  created_at=None, updated_at=None, description=None):
         self.service_type_id = service_type_id
         self.name = name
         self.description = description
+        self.created_at = created_at or datetime.utcnow()
+        self.updated_at = updated_at or datetime.utcnow()
 
     @classmethod
     def from_db_row(cls, row):
-        service_type_id, name, description = row
-        return cls(service_type_id, name, description)
+        service_type_id, name, description, created_at, updated_at = row
+        return cls(service_type_id, name, description, created_at, updated_at)
